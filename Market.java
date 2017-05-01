27c7bd9adc07ff824ee0db8087a3720b4b55bba9
Byron Blocker <byronblocker@gmail.com>
2017-05-01 13:51:00 -0700

    program to print grocery items

---
+++ Market.java
@@ -0,0 +1,9 @@
+public class Market {
+  public static void main(String[] args){
+    String[] list = {"Sriracha","trash bags", "soup"};
+
+    for (String item : list){
+      System.out.println("Don't forget to grab " + item + "!");
+    }
+  }
+}
