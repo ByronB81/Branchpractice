78a66fea177c2cf04583b8f034d51c9e5892c475
Byron Blocker <byronblocker@gmail.com>
2017-05-01 13:47:19 -0700

    program to list super heros

---
+++ LoveHate.java
@@ -0,0 +1,10 @@
+
+public class LoveHate {
+  public static void main(String[] args) {
+    String[] heros = {"Batman","Superman","The Flash"};
+
+    for (String hero : heros) {
+      System.out.println(hero + " is protecting the city");
+    }
+  }
+}
