9313c11fc1b23d14a301311f754c9d3bce62f5bf
Byron Blocker <byronblocker@gmail.com>
2017-05-01 13:59:05 -0700

    minor changes

--- Market.java
+++ Market.java
@@ -1,9 +1,16 @@
 public class Market {
   public static void main(String[] args){
     String[] list = {"Sriracha","trash bags", "soup"};
+    Integer[] prices = {3,6,2};
 
     for (String item : list){
       System.out.println("Don't forget to grab " + item + "!");
     }
+
+    Integer total = 0;
+    for (Integer price : prices) {
+      total += price;
+    }
+    System.out.println("Should cost " + total + " dollars.");
   }
 }
